5874a0be208b7c2e8271b2b447f0439d3a4dad0b
Mateusz Dukat <mateusz.dukat1@gmail.com>
2021-09-27 22:49:09 +0200

    Create strVSchr.toUpperCase.toLowerCase.java

---
+++ strVSchr.toUpperCase.toLowerCase.java
@@ -0,0 +1,14 @@
+public class Strvschr{
+
+     public static void main(String []args){
+        
+        // With strings, You operate on objects
+        String str = "ThIs Is A StRiNg";
+        String str2 = str.toLowerCase();
+        
+        // But with chars, You operate on variable as argument
+        char chr = 'A';
+        char chr2 = Character.toLowerCase(chr);
+       
+     }
+}
